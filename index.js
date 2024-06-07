1957648919160146de216f21ccbd2b392e7ca436
huzefarana <imhuzefarana@gmail.com>
2024-06-07 18:44:35 +0500

    fix CORS

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const mainController = require("./controller");
-
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
